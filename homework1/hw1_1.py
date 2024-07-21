46c3f7478753963df7a8d506d99f2399e58ab7ff
Alexander A. Barkan <albarkan@inbox.ru>
2024-07-21 20:07:28 +0300

    Add files via upload

--- homework1/hw1_1.py
+++ homework1/hw1_1.py
@@ -1,1 +1,6 @@
-
+try:
+    n = int(input('Input number of rows. '))
+    for i in range(n):
+        print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+except Exception:
+    print("Incorrect input. Please try again.")
