180ce721a07732432ea193fc288932caa661400a
Alexander A. Barkan <askbarkan@gmail.com>
2024-07-22 16:09:31 +0300

    Update barkan_ht4_3.py

--- homework4/homework4_3/barkan_ht4_3.py
+++ homework4/homework4_3/barkan_ht4_3.py
@@ -1,27 +1,23 @@
 def count_ways(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1
-    elif n == 1:
-        return 1
-    else:
-        ways = [0] * (n + 1)
-        ways[0] = 1
-        ways[1] = 1
 
-        for i in range(2, n + 1):
-            ways[i] = ways[i - 1] + ways[i - 2]
+    ways = [0] * (n + 1)
+    ways[0] = 1
+    ways[1] = 1
 
-        return ways[n]
+    for i in range(2, n + 1):
+        ways[i] = ways[i - 1] + ways[i - 2]
+
+    return ways[n]
 
 try:
     n = int(input("Введите количество ступенек (n): "))
     if n < 0:
         print("Число ступенек должно быть неотрицательным.")
-
-    ways = count_ways(n)
-
-    print(f"Количество способов подняться на {n} ступенек: {ways}")
+    else:
+        ways = count_ways(n)
+        print(f"Количество способов подняться на {n} ступенек равняется: {ways}")
 
 except ValueError:
-    print("Ошибка: введите целое число.")
-
+    print("Ошибка: введите натуральое число.")
