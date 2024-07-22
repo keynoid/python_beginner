0aba609e42c1fefcad696673d9c2d35b4d2c3b82
Alexander A. Barkan <askbarkan@gmail.com>
2024-07-22 14:50:58 +0300

    Delete hw1_1.py

--- hw1_1.py
+++
@@ -1,6 +0,0 @@
-try:
-    n = int(input('Input number of rows. '))
-    for i in range(n):
-        print(' ' * (n - i - 1) + '*' * (2 * i + 1))
-except Exception:
-    print("Incorrect input. Please try again.")
